9673a56d44da7fe1f1f5470ea3f48bfe78aeae05
Eleanor Tosh <eleanortosh@outlook.com>
2023-03-15 18:01:50 +1300

    got rid of the white line under footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Footer() {
   return (
-<div className='container px-10 flex flex-row justify-center items-center mx-auto bg-grey-ish border-t-2 border-text-grey py-10 mb-10'>
+<div className='container px-10 flex flex-row justify-center items-center mx-auto bg-grey-ish border-t-2 border-text-grey py-10'>
     <a href='/'><i className="fa-brands fa-facebook p-5 font-bold text-text-grey text-4xl"></i></a>
   <a href='/'><i className="fa-brands fa-instagram p-5 font-bold text-text-grey text-4xl"></i></a>
   <a href='/'><i className="fa-brands fa-twitter p-5 font-bold text-text-grey text-4xl"></i></a>
